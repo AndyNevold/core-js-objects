f273678ab557ba259bced1227683ebdd5835f6e4
AndyNevold <andynevold@gmail.com>
2025-12-05 12:26:47 +0300

    feat: add some tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -133,8 +133,15 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const arrKeys = Object.keys(lettersObject);
+  const resArr = Array(Object.values(lettersObject).flat().length);
+  arrKeys.forEach((el) => {
+    lettersObject[el].forEach((elem) => {
+      resArr[elem] = el;
+    });
+  });
+  return resArr.join('');
 }
 
 /**
@@ -151,8 +158,15 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let resultChange = 0;
+  function isSellingTickets(ticketCost) {
+    if (ticketCost - 25 <= resultChange) {
+      resultChange += 25;
+    }
+    return resultChange >= ticketCost - 25;
+  }
+  return queue.every(isSellingTickets);
 }
 
 /**
@@ -168,8 +182,15 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  const rectangle = Object.create({
+    getArea() {
+      return this.width * this.height;
+    },
+  });
+  rectangle.width = width;
+  rectangle.height = height;
+  return rectangle;
 }
 
 /**
@@ -182,8 +203,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { height: 10, width: 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
